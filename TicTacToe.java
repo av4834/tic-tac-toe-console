52b14fb6ae820c0f5f270968781a2401701cd47f
Akhil Vulavakayala <av4834@srmist.edu.in>
2026-04-20 16:11:18 +0530

    UC2: Toss to decide first player and assign symbols

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,19 +1,40 @@
+import java.util.Random;
+
 public class TicTacToe {
 
-    static char[][] board = {
-        {'-', '-', '-'},
-        {'-', '-', '-'},
-        {'-', '-', '-'}
-    };
+    static boolean isHumanTurn;
+    static char humanSymbol;
+    static char computerSymbol;
 
     public static void main(String[] args) {
-        System.out.println(isValidMove(1, 1));
+        tossAndAssignSymbols();
+        displayTossResult();
     }
 
-    static boolean isValidMove(int row, int col) {
-        if (row >= 0 && row < 3 && col >= 0 && col < 3) {
-            return board[row][col] == '-';
+    static void tossAndAssignSymbols() {
+        Random random = new Random();
+        int toss = random.nextInt(2);
+
+        if (toss == 0) {
+            isHumanTurn = true;
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+        } else {
+            isHumanTurn = false;
+            humanSymbol = 'O';
+            computerSymbol = 'X';
         }
-        return false;
+    }
+
+    static void displayTossResult() {
+        System.out.println("--- Toss Result ---");
+        if (isHumanTurn) {
+            System.out.println("Human won the toss and plays first!");
+        } else {
+            System.out.println("Computer won the toss and plays first!");
+        }
+        System.out.println("Human Symbol: " + humanSymbol);
+        System.out.println("Computer Symbol: " + computerSymbol);
+        System.out.println("-------------------");
     }
 }
